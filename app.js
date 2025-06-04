0298e1046d80f2d02105c3fe5d438b4c03fef4a9
Jason Wilson <jrwilson89@gmail.com>
2025-06-04 15:41:24 -0500

    working to fix createClient bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    supabase = supabase.createClient(
+    supabase = createClient(
         "https://xfxmqrvghvdqjtzxyvqa.supabase.co",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmeG1xcnZnaHZkcWp0enh5dnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTMzMDgsImV4cCI6MjA2NDAyOTMwOH0.cHZiSlljOSxf6DNTBQGs6cQd1UXROUTQLZvcn8kMdyg"
     );
@@ -17,14 +17,14 @@ form.addEventListener("submit", async (event) => {
         }
     }
 
-    const { error, data } = await supabase
-        .from("solac_visitor")
-        .insert(submission);
-    if (error) {
-        alert("Please fill out required fields.");
-    }
-        else {
-            alert("Thank you for visiting us!  May God continue to bless you!");
-        }
+    // const { error, data } = await supabase
+    //     .from("solac_visitor")
+    //     .insert(submission);
+    // if (error) {
+    //     alert("Please fill out required fields.");
+    // }
+    //     else {
+    //         alert("Thank you for visiting us!  May God continue to bless you!");
+    //     }
 
 });
