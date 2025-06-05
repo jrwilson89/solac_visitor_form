42e6fdcbfeb4b8b1d651b84b95595f67e2a24e72
Jason Wilson <jrwilson89@gmail.com>
2025-06-05 12:29:58 -0500

    corrected createClient error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    supabase = createClient(
+    const storage = supabase.createClient(
         "https://xfxmqrvghvdqjtzxyvqa.supabase.co",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmeG1xcnZnaHZkcWp0enh5dnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTMzMDgsImV4cCI6MjA2NDAyOTMwOH0.cHZiSlljOSxf6DNTBQGs6cQd1UXROUTQLZvcn8kMdyg"
     );
@@ -10,19 +10,26 @@ form.addEventListener("submit", async (event) => {
 
 
     const submission = {};
+
     for (let element of form.elements) {
         if (element.name) {
-            submission[element.name] = element.value;
+            if (element.type === "checkbox") {
+                if (element.checked) {
+                    submission[element.name] = element.value;
+                }
+            } else {
+                submission[element.name] = element.value;
+            }
             element.value = "";
         }
     }
 
-    const { error, data } = await supabase
+    const { error, data } = await storage
         .from("solac_visitor")
         .insert(submission);
-    // if (error) {
-    //     alert("Please fill out required fields.");
-    // }
+    if (error) {
+       console.error(error);
+    }
     //     else {
     //         alert("Thank you for visiting us!  May God continue to bless you!");
     //     }
